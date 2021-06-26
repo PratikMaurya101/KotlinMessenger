11ed2dee51e1db15b3abab9fe1fd1e9089ae2b88
PratikMaurya <pratikmaurya101@gmail.com>
2021-06-26 17:46:00 +0530

    11th
    - added permission to read contacts from device
    - added functions in NewChatActivity.kt to fetchUsers from database [yet to be fully implemented]

--- app/src/main/java/com/example/kotlinmessenger/NewChatActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/NewChatActivity.kt
@@ -2,7 +2,12 @@ package com.example.kotlinmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.kotlinmessenger.databinding.ActivityNewChatBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.xwray.groupie.GroupieAdapter
 
 class NewChatActivity : AppCompatActivity() {
@@ -25,10 +30,26 @@ class NewChatActivity : AppCompatActivity() {
         // [setting the cardView to the recycler view adapter]
         binding.recyclerviewNewchat.setAdapter(adapter)
 
+        fetchUsers()
 
+    }
 
+    private fun fetchUsers() {
+        val userReference = FirebaseDatabase.getInstance().getReference("/users")
 
+        // research about this later
+        userReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.children.forEach { userDetails ->
+                    Log.d("NewChatActivity","USER:$userDetails")
+                }
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 }
 
